903e91a58106a0a65572bca79f82bcf37ab28911
DakshitaJain0212 <dakshitaj02@gmail.com>
2023-09-25 11:49:30 +0530

    Update and New Files

--- src/App.js
+++ src/App.js
@@ -1,29 +1,31 @@
-import React, {useRef} from "react";
+import React, { useRef } from "react";
 import "./App.css";
 import SpeechRecognition, {
   useSpeechRecognition,
 } from "react-speech-recognition";
 
-function App() {
+function App() { 
 
-  const { transcript , resetTranscript } = useSpeechRecognition()
+  // hook to reset the transcript
+  const { transcript, resetTranscript } = useSpeechRecognition();
 
-
-  const { browserSupportsSpeechRecognition } =
-    useSpeechRecognition();
+  // hook to start the listening
+  const { browserSupportsSpeechRecognition } = useSpeechRecognition();
 
   const startListening = () =>
-    SpeechRecognition.startListening({ continuous: true });
-    
-const divRef = useRef();
-
-const CopyToClipboard = () => {
-  const textToCopy = divRef.current.innerText;
-   
-  navigator.clipboard.writeText(textToCopy);
-}
+    SpeechRecognition.startListening({ continuous: true , language: 'en-IN'});
 
 
+    // copy to clipboard 
+  const divRef = useRef();
+
+  const CopyToClipboard = () => {
+    const textToCopy = divRef.current.innerText;
+
+    navigator.clipboard.writeText(textToCopy);
+  };
+
+  // check browserSupportsSpeechRecognition
   if (!browserSupportsSpeechRecognition) {
     return null;
   }
@@ -34,14 +36,19 @@ const CopyToClipboard = () => {
         <h2>Speech to Text Convertor</h2>
         <br />
         <p>
-          Transform Spoken Words into Text Effortlessly and Simplify Your Workflow. Your Voice, Our Typing!
+          Transform Spoken Words into Text Effortlessly and Simplify Your
+          Workflow. Your Voice, Our Typing!
         </p>
       </div>
 
       <div className="main-content">
-      <i className="fa-sharp fa-solid fa-arrow-rotate-right" onClick={resetTranscript}></i>
-        <div className="content" ref={divRef}>{transcript}</div>
-
+        <i
+          className="fa-sharp fa-solid fa-arrow-rotate-right"
+          onClick={resetTranscript}
+        ></i>
+        <div className="content" ref={divRef}>
+          {transcript}
+        </div>
       </div>
 
       <div className="btn-style">
